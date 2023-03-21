f8d5badf6096ce5aa00d1d13a1ab136249bf1aa7
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-03-21 07:14:45 +0100

    Add doc comment for the functions

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""Module containing the pascal triangle generator
 
-def pascal_helper(array, current, stop):
+"""
+
+def _pascal_helper(array, current, stop):
+    """ Recursive pascal helper
+
+    Args:
+        array (list): List containing the total output lists.
+        current (int): Current iteration of the triangle.
+        stop (int): the last iteration of the triangle.
+
+    Return:
+        None: Once completed.
+        _pascal_helper (Function): If the iterations are not done.
+
+    """
     if current == 1:
         array.append([1])
     elif current == 2:
@@ -23,13 +38,22 @@ def pascal_helper(array, current, stop):
     if current == stop:
         return
     else:
-        return pascal_helper(array, current + 1, stop)
+        return _pascal_helper(array, current + 1, stop)
             
 
 def pascal_triangle(n):
+    """ Pascal triangle generator
+
+    Args:
+        n (int): Number of iterations of the triangle.
+
+    Return:
+        list of lists: A list containing the triangle matrix.
+
+    """
     if n <= 0:
         return []
     else:
         arr = []
-        pascal_helper(arr, 1, n);
+        _pascal_helper(arr, 1, n);
         return arr
