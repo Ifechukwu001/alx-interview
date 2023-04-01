97bc00fcb3d15b50da16380668e42ca1747ce254
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-01 04:43:15 +0100

    Combine to one function

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,23 +5,6 @@ The Algorithm has a time complexity of O(n)
 """
 
 
-def openBox(boxes: list[list[int]], box: int, key_list: list[int]) -> bool:
-    """Opens a box
-
-    Args:
-        boxes (list of list of int): Box list.
-        box (int): Box number.
-        key_list (list of int): List of box keys.
-
-    Returns:
-        bool: True if opened , False otherwise
-    """
-    if box in key_list:
-        key_list.extend(boxes[box])
-        return True
-    return False
-
-
 def canUnlockAll(boxes: list[list[int]]) -> bool:
     """Can Unlock All
 
@@ -34,20 +17,21 @@ def canUnlockAll(boxes: list[list[int]]) -> bool:
     opened_list: list[int] = []
     closed_list: list[int] = []
     key_list: list[int] = [0]
+    boxes_len = len(boxes)
 
-    for box in range(len(boxes)):
-        if openBox(boxes, box, key_list):
+    for box in range(boxes_len):
+        if box in key_list:
+            key_list.extend(boxes[box])
             opened_list.append(box)
         else:
             closed_list.append(box)
 
     if closed_list:
         for box in closed_list.copy():
-            if openBox(boxes, box, key_list):
-                opened_list.append(box)
+            if box in key_list:
+                key_list.extend(boxes[box])
                 closed_list.remove(box)
 
     if len(closed_list) > 0:
         return False
-    else:
-        return True
+    return True
