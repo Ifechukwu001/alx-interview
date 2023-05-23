e72e44a1d910325e909023bd6cc0d71a4f296a73
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-05-23 11:32:40 +0100

    Solution to the interview question

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Solution to interview question"""
+
+
+def island_perimeter(grid):
+    """Calculates perimeter of an island
+    Returns:
+        int: Perimeter
+    """
+    grid_length = len(grid)
+    len_count = 0
+    prev_box = 0
+
+    perimeter = 0
+
+    for row in grid:
+        for idx, col in enumerate(row):
+            if col > prev_box:
+                perimeter += 1
+            if col < prev_box:
+                perimeter += 1
+            if len_count and grid[len_count - 1][idx] < col:
+                perimeter += 1
+            if len_count < grid_length - 1 and grid[len_count + 1][idx] < col:
+                perimeter += 1
+            prev_box = col
+        len_count += 1
+    return perimeter
